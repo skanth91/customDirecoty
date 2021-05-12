7e829efff4064562bebbf60eaad2535286197b52
Srikanth <eminosoft.srikanth@git.com>
2021-05-12 14:40:42 +0530

    Update dirAttribDirective.ts

--- dirAttribDirective.ts
+++ dirAttribDirective.ts
@@ -3,32 +3,32 @@ import { Directive, Input, TemplateRef, ViewContainerRef } from "@angular/core";
 
 @Directive({
     selector: '[dirAttrConfig]'
-  })
-  export class DirAttribDirective {
+})
+export class DirAttribDirective {
     @Input() dirAttrConfig: string | string[];
     private httpClient: HttpClient;
     private featureFlag!: {
         enabled: any
-      }
+    }
     constructor(
-      private vcr: ViewContainerRef,
-      private tpl: TemplateRef<any>,
-      httpClient: HttpClient
+        private vcr: ViewContainerRef,
+        private tpl: TemplateRef<any>,
+        httpClient: HttpClient
     ) {
-        this.httpClient =httpClient;
+        this.httpClient = httpClient;
     }
 
-    public async ngOnInit() : Promise<void> {
-        try{
-          var remoteConfig = await this.httpClient
-          .get<DirAttribDirective>("assets/config.json").toPromise();
-          if (remoteConfig.featureFlag.enabled == this.dirAttrConfig) {
-            this.vcr.createEmbeddedView(this.tpl);
-          }
-          
-        } catch(err) {
-          console.log(err)
+    public async ngOnInit(): Promise<void> {
+        try {
+            var remoteConfig = await this.httpClient
+                .get<DirAttribDirective>("assets/config.json").toPromise();
+            if (remoteConfig.featureFlag.enabled == this.dirAttrConfig) {
+                this.vcr.createEmbeddedView(this.tpl);
+            }
+
+        } catch (err) {
+            console.log(err)
         }
-      }
-  
-  }
+    }
+
+}
