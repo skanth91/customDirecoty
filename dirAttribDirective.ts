9f8c6b0ff4da6413ba966a06ca090cadbf5e7c1a
Srikanth <eminosoft.srikanth@git.com>
2021-05-12 14:40:21 +0530

    Update dirAttribDirective.ts

--- dirAttribDirective.ts
+++ dirAttribDirective.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from "@angular/common/http";
 import { Directive, Input, TemplateRef, ViewContainerRef } from "@angular/core";
-import { AppConfig } from "./app.config.service";
 
 @Directive({
     selector: '[dirAttrConfig]'
@@ -8,10 +7,12 @@ import { AppConfig } from "./app.config.service";
   export class DirAttribDirective {
     @Input() dirAttrConfig: string | string[];
     private httpClient: HttpClient;
+    private featureFlag!: {
+        enabled: any
+      }
     constructor(
       private vcr: ViewContainerRef,
       private tpl: TemplateRef<any>,
-      private appConfig: AppConfig,
       httpClient: HttpClient
     ) {
         this.httpClient =httpClient;
@@ -20,7 +21,7 @@ import { AppConfig } from "./app.config.service";
     public async ngOnInit() : Promise<void> {
         try{
           var remoteConfig = await this.httpClient
-          .get<AppConfig>("assets/config.json").toPromise();
+          .get<DirAttribDirective>("assets/config.json").toPromise();
           if (remoteConfig.featureFlag.enabled == this.dirAttrConfig) {
             this.vcr.createEmbeddedView(this.tpl);
           }
